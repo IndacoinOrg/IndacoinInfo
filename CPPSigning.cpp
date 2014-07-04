43a0ef5ff60f9fe069f2af77b65cd89c00828d7e
UPelsin.MAPAT@gmail.com <UPelsin.MAPAT@gmail.com>
2014-07-04 11:23:06 +0400

    Signing Examples

--- CPPSigning.cpp
+++ CPPSigning.cpp
@@ -12,6 +12,11 @@ static const std::string base64_chars =
              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
              "abcdefghijklmnopqrstuvwxyz"
              "0123456789+/";
+			 
+std::string ecdsaSha1(unsigned char * Sign,const unsigned char * PrivateKey);			
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len);
+std::string base64_decode(std::string const& encoded_string);
+static inline bool is_base64(unsigned char c) ;
 
 std::string ecdsaSha1(unsigned char * Sign,const unsigned char * PrivateKey){
 	
